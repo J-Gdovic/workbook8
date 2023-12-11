9594c3a17d3f6b76a5b6ed25b109a10a020504da
J Gdovic <jgdovic3@gmail.com>
2023-12-11 15:53:57 -0500

    learn to push using fetch

--- demos/fetch/users.js
+++ demos/fetch/users.js
@@ -7,26 +7,30 @@
 
 //GET ALL Await function
 async function getUsers() {
-    const response = await fetch("http://localhost:3000/users");
-    const users = await response.json()
-    return users;
+  const response = await fetch("http://localhost:3000/users");
+  const users = await response.json();
+  return users;
 }
 
-async function init(){
-const users = await getUsers();
-console.log(users);
+async function init() {
+  const users = await getUsers();
+  console.log(users);
 }
 init();
 
-
 // POST
+const newUSER = {
+  name: "Kanye WEST",
+  username: "YEEZYYE",
+  email: "KW@fakeit.com",
+};
 
-fetch("http://localhost:3000/users",{method: 'POST',
-headers:'',
-body:
-
+fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(newUSER)
 })
-.then((response) => response.json())
-.then((users) => {
+  .then((response) => response.json())
+  .then((users) => {
     console.log(users);
-})
+  });
