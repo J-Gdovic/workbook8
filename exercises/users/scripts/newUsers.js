75dd55d3b3ff6f34b5b81debca97bc2387b0e5cd
J Gdovic <jgdovic3@gmail.com>
2023-12-12 14:30:46 -0500

    created function to add new user through form!

--- exercises/users/scripts/newUsers.js
+++ exercises/users/scripts/newUsers.js
@@ -1,18 +1,43 @@
-const addedUser ={
-    name:"Sidney Crosby",
-    email:"sidTHAkid87@pens.net",
-    username: "sidneyKidney877887",
-    street:"BarDown Lane",
-    city:"Pittsburgh",
+let nameFeild = document.querySelector("#nameFeild");
+let emailFeild = document.querySelector("#emailFeild");
+let userNameFeild = document.querySelector("#userNameFeild");
+let streetFeild = document.querySelector("#streetFeild");
+let cityFeild = document.querySelector("#cityFeild");
+
+let inputFeildButton = document.querySelector("#inputFeildButton");
+
+
+
+
+
+function addNewUser() {
+    let newNameFeild = nameFeild.value;
+    let newEmailFeild = emailFeild.value;
+    let newUserNameFeild = userNameFeild.value;
+    let newStreetFeild = streetFeild.value;
+    let newCityFeild = cityFeild.value;
+
+const addedUser = {
+  name: `${newNameFeild}`,
+  email: `${newEmailFeild}`,
+  username: `${newUserNameFeild}`,
+  address:{
+    street: `${newStreetFeild}`,
+    city: `${newCityFeild}`,
+  }
+  
+};
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(addedUser),
+  })
+    .then((response) => response.json())
+    .then((addedUser) => {
+      // console.log(addedUser);
+      return addedUser;
+    });
 }
 
-fetch("http://localhost:3000/users", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify(addedUser),
-})
-  .then((response) => response.json())
-  .then((addedUser) => {
-    console.log(addedUser);
-  });
-  
+//wire up
+inputFeildButton.onclick = addNewUser;
