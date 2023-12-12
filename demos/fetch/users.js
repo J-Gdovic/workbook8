dc603f41b786f8be24908654f6a0c79ce842557b
J Gdovic <jgdovic3@gmail.com>
2023-12-12 09:25:24 -0500

    return users

--- demos/fetch/users.js
+++ demos/fetch/users.js
@@ -1,22 +1,22 @@
-//GET ALL
-// fetch("http://localhost:3000/users")
-// .then((response) => response.json())
-// .then((users) => {
-//     console.log(users);
-// })
+//GET ALL Fetch
+fetch("http://localhost:3000/users")
+  .then((response) => response.json())
+  .then((users) => {
+    console.log(users);
+  });
 
 //GET ALL Await function
-async function getUsers() {
-  const response = await fetch("http://localhost:3000/users");
-  const users = await response.json();
-  return users;
-}
+// async function getUsers() {
+//   const response = await fetch("http://localhost:3000/users");
+//   const users = await response.json();
+//   return users;
+// }
 
-async function init() {
-  const users = await getUsers();
-  console.log(users);
-}
-init();
+// async function init() {
+//   const users = await getUsers();
+//   console.log(users);
+// }
+// init();
 
 // POST
 const newUSER = {
@@ -28,9 +28,9 @@ const newUSER = {
 fetch("http://localhost:3000/users", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
-  body: JSON.stringify(newUSER)
+  body: JSON.stringify(newUSER),
 })
   .then((response) => response.json())
   .then((users) => {
-    console.log(users);
+    return users;
   });
