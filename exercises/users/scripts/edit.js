7286f61ece12633bfaadd55a1c035da0f68012b0
J Gdovic <jgdovic3@gmail.com>
2023-12-13 14:52:47 -0500

    got edit working and redirecting back to userhtml

--- exercises/users/scripts/edit.js
+++ exercises/users/scripts/edit.js
@@ -13,28 +13,46 @@ function editUserData() {
   let id = -1;
   if (urlParams.has("id") === true) {
     id = urlParams.get("id");
-    fetch(`http://localhost:3000/users/${id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(user),
-    })
+    fetch(`http://localhost:3000/users/${id}`)
       .then((response) => response.json())
       .then((user) => {
-        return user;
+        nameFeild.value = user.name;
+        emailFeild.value = user.email;
+        userNameFeild.value = user.username;
+        streetFeild.value = user.address.street;
+        cityFeild.value = user.address.city;
       });
   }
 }
+function editUser() {
+  let newNameFeild2 = nameFeild.value;
+  let newEmailFeild2 = emailFeild.value;
+  let newUserNameFeild2 = userNameFeild.value;
+  let newStreetFeild2 = streetFeild.value;
+  let newCityFeild2 = cityFeild.value;
 
-function populateUserData() {
-  if (user.id == user.name) {
-    nameFeild.innerText = user.name;
-    emailFeild.innerText = user.email;
-    userNameFeild.innerText = user.username;
-    streetFeild.innerText = user.address.street;
-    cityFeild.innerText = user.address.city;
-  }
+  const editedUser = {
+    name: `${newNameFeild2}`,
+    email: `${newEmailFeild2}`,
+    username: `${newUserNameFeild2}`,
+    //had to add new object to get it listed in table
+    address: {
+      street: `${newStreetFeild2}`,
+      city: `${newCityFeild2}`,
+    },
+  };
+  let id = urlParams.get("id");
+  fetch(`http://localhost:3000/users/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(editedUser),
+  })
+    .then((response) => response.json())
+    .then((editedUser) => {
+      window.location.href = "users.html";
+    });
 }
 
 //wire up
-populateUserData();
-inputFeildButton.onclick = editUserData;
+editUserData();
+inputFeildButton.onclick = editUser;
