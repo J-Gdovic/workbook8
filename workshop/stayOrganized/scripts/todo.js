47b2e564a19748a2f16aca07dd28940af7773452
J Gdovic <jgdovic3@gmail.com>
2023-12-14 16:01:33 -0500

    able to make fetch but still need to get for loop for userid

--- workshop/stayOrganized/scripts/todo.js
+++ workshop/stayOrganized/scripts/todo.js
@@ -3,5 +3,23 @@
 let userIdSelect = document.querySelector("#userIdSelect");
 let todoTbody = document.querySelector("#todoTbody");
 
+//Put userid in drop down!, fetch todoArray
+function loadTodoArray() {
+  fetch("http://localhost:8083/api/todos")
+    .then((response) => response.json())
+    .then((todoArray) => {
+      todoArray;
+      //   console.log(todoArray);
+    });
+}
+loadTodoArray();
 
-//Put userid in drop down!, fetch data
+function selectByUserId() {
+  for (const user of users) {
+    let option = document.createElement("option");
+    option.value = user.id;
+    option.innerText = user.userid;
+    userIdSelect.appendChild(option);
+  }
+}
+selectByUserId();
