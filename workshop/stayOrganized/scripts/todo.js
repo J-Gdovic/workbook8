fde6ceb95ca45b29fcc5314532bfb93478d21a9d
J Gdovic <jgdovic3@gmail.com>
2023-12-14 15:16:50 -0500

    refresh

--- workshop/stayOrganized/scripts/todo.js
+++ workshop/stayOrganized/scripts/todo.js
@@ -1,2 +1,7 @@
 "use strict";
-//
+//variables
+let userIdSelect = document.querySelector("#userIdSelect");
+let todoTbody = document.querySelector("#todoTbody");
+
+
+//Put userid in drop down!, fetch data
