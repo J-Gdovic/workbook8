4e7caff6fada47e5c774ec73a84b692b722075c8
J Gdovic <jgdovic3@gmail.com>
2023-12-15 11:44:08 -0500

    did fetch for todo table to display per user

--- workshop/stayOrganized/scripts/todos.js
+++ workshop/stayOrganized/scripts/todos.js
@@ -18,25 +18,29 @@ function loadTodoArray() {
 }
 
 function buildTodoTable() {
-  todoTbody.innerText = "";
-  let selectedUserId = userIdSelect.value;
-  for (const user of users) {
-    if (user.id == selectedUserId) {
-      const tr = todoTbody.insertRow(-1);
+  fetch("http://localhost:8083/api/todos")
+    .then((response) => response.json())
+    .then((todos) => {
+      todoTbody.innerText = "";
+      let selectedUserId = userIdSelect.value;
+      for (const todo of todos) {
+        if (selectedUserId == todo.userid) {
+          const tr = todoTbody.insertRow(-1);
 
-      let td1 = tr.insertCell(0);
-      td1.innerText = user.category;
+          let td1 = tr.insertCell(0);
+          td1.innerText = todo.category;
 
-      let td2 = tr.insertCell(1);
-      td2.innerText = user.description;
+          let td2 = tr.insertCell(1);
+          td2.innerText = todo.description;
 
-      let td3 = tr.insertCell(2);
-      td3.innerText = user.deadline;
+          let td3 = tr.insertCell(2);
+          td3.innerText = todo.deadline;
 
-      let td4 = tr.insertCell(3);
-      td4.innerText = user.priority;
-    }
-  }
+          let td4 = tr.insertCell(3);
+          td4.innerText = todo.priority;
+        }
+      }
+    });
 }
 loadTodoArray();
 userIdSelect.onclick = buildTodoTable;
