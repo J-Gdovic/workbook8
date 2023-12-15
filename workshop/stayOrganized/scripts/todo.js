b329b66cb11e6728a1b20491bc38f2f89ba326ad
J Gdovic <jgdovic3@gmail.com>
2023-12-15 09:22:29 -0500

    got table working

--- workshop/stayOrganized/scripts/todo.js
+++ workshop/stayOrganized/scripts/todo.js
@@ -2,24 +2,45 @@
 //variables
 let userIdSelect = document.querySelector("#userIdSelect");
 let todoTbody = document.querySelector("#todoTbody");
-
+let todoArray;
 //Put userid in drop down!, fetch todoArray
 function loadTodoArray() {
   fetch("http://localhost:8083/api/todos")
     .then((response) => response.json())
-    .then((todoArray) => {
-      todoArray;
-      //   console.log(todoArray);
+    .then((todos) => {
+        todoArray = todos;//assigns the fectch to the golbal var
+      for (const user of todoArray) {
+        let option = document.createElement("option");
+        option.value = user.id;
+        option.innerText = user.userid;
+        userIdSelect.appendChild(option);
+      }
+      
     });
 }
 loadTodoArray();
 
-function selectByUserId() {
-  for (const user of users) {
-    let option = document.createElement("option");
-    option.value = user.id;
-    option.innerText = user.userid;
-    userIdSelect.appendChild(option);
+function buildTodoTable() {
+  todoTbody.innerText = "";
+  const selectedUserId = userIdSelect.value
+  for (const user of todoArray) {
+    if (user.userid == selectedUserId) {
+      const tr = todoTbody.insertRow(-1);
+
+      let td1 = tr.insertCell(0);
+      td1.innerText = user.category;
+
+      let td2 = tr.insertCell(1);
+      td2.innerText = user.description;
+
+      let td3 = tr.insertCell(2);
+      td3.innerText = user.deadline;
+
+      let td4 = tr.insertCell(3);
+      td4.innerText = user.priority;
+    }
   }
 }
-selectByUserId();
+
+userIdSelect.onclick = buildTodoTable;
+
